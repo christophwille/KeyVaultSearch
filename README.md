67ec50ef0f20b2d51d93be93d5630f2c16ba2b4c
Christoph Wille <christoph.wille@gmail.com>
2022-09-21 15:35:31 +0200

    Add the basic KeyVaultSearch tool

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# KeyVaultSearch
-Search in Azure Key Vault using DefaultAzureCredential
+# Search in Azure Key Vault using DefaultAzureCredential
+
+I got annoyed that I can't search for secrets in the Azure Portal... so I built myself a super-simple tool to do just that.
+
+![how to search](searchexperience.png)
+
+Note it uses DefaultAzureCredential and nothing else.
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Windows;
+
+namespace KeyVaultSearch
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.kvSecretValue.Text = "loading...";
+            var client = new SecretClient(new Uri(this.kvUri.Text), new DefaultAzureCredential());
+
+            try
+            {
+                KeyVaultSecret theSecret = await client.GetSecretAsync(this.kvSecret.Text);
+                this.kvSecretValue.Text = theSecret.Value;
+            }
+            catch (Exception ex)
+            {
+                this.kvSecretValue.Text = ex.Message;
+            }
+        }
+    }
+}
