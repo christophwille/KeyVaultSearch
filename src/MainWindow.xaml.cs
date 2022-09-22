e0ac9b5b6fba562ed64bbade156f628e27512b15
Christoph Wille <christoph.wille@gmail.com>
2022-09-22 07:09:01 +0200

    Optimize DefaultAzureCredential

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -18,7 +18,20 @@ namespace KeyVaultSearch
 
             try
             {
-                var client = new SecretClient(new Uri(this.kvUri.Text), new DefaultAzureCredential());
+                //
+                // Optimize for your use case via https://scottsauber.com/2022/05/10/improving-azure-key-vault-performance-in-asp-net-core-by-up-to-10x/
+                //
+                var client = new SecretClient(new Uri(this.kvUri.Text), new DefaultAzureCredential(new DefaultAzureCredentialOptions
+                {
+                    ExcludeAzurePowerShellCredential = false,
+                    ExcludeAzureCliCredential = true,
+                    ExcludeEnvironmentCredential = true,
+                    ExcludeInteractiveBrowserCredential = true,
+                    ExcludeSharedTokenCacheCredential = true,
+                    ExcludeVisualStudioCodeCredential = true,
+                    ExcludeVisualStudioCredential = true,
+                    ExcludeManagedIdentityCredential = true,
+                }));
                 KeyVaultSecret theSecret = await client.GetSecretAsync(this.kvSecret.Text);
                 this.kvSecretValue.Text = theSecret.Value;
             }
